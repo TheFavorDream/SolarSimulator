73fb5a5f166d6033156d0e393718468a198a6ea2
fevordream <fevordream@gmail.com>
2025-07-23 11:13:30 +0330

    Init

---
+++ SolarSimulator/Source/main.cpp
@@ -0,0 +1,21 @@
+/*
+
+	Created and Developed by Pooya Alizadeh (aka TheVoltage)
+
+	Purpose:
+		SolarSimulator is a computer program designed to run on windows (at least for now) for simulating our solar system 
+		and mainly The Universal Gravity Law.
+		Made with C++ and OpenGL as well as some other libraries like glfw and etc.
+*/
+
+
+
+#include <iostream>
+
+
+int main(int argc, char** arvg)
+{
+	std::cout << "Hello, World\n";
+	std::cin.get();
+	return 0;
+}
